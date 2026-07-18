fd30f1677df3814fbcef2ec5a14473cf310f2cd3
wa110573ter <wa110573ter@gmail.com>
2026-07-18 21:25:01 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='tw-water-v1',A=['./','./index.html','./jszip.min.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
