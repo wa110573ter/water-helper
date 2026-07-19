f736e81dc7bcf1478eba6260ec583cb9ec69d85a
wa110573ter <wa110573ter@gmail.com>
2026-07-19 10:33:12 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='water-helper-v12-20260719';
-const ASSETS=['./?v=12','./index.html?v=12','./manifest.webmanifest?v=12','./icon-192.png?v=12','./icon-512.png?v=12'];
+const CACHE='water-helper-v13-20260719';
+const ASSETS=['./?v=13','./index.html?v=13','./manifest.webmanifest?v=13','./icon-192.png?v=13','./icon-512.png?v=13'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
